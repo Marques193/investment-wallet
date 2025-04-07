bb8d945afb485ead3828230e40614e7ffab93a74
marques <gustavosmarques19@gmail.com>
2025-04-07 20:22:46 -0300

    add script file

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+// Tabela de ações
+const stockTable = document.querySelector("#stock-table");
+
+const stockName = document.querySelector(".stock-name");
+const stockAmount = document.querySelector(".stock-amount");
+const stockBalance = document.querySelector("#stock-balance");
+const stockPrice = document.querySelector("#stock-price");
+const stockAverage = document.querySelector("#stock-average");
+const stockVar = document.querySelector("#stock-var");
+const stockDy = document.querySelector("#stock-dy");
